232c6ce97a3053ce86065e61f36e63d33f6512e0
lowhanfish <lowhanfish@gmail.com>
2026-07-31 19:58:06 +0800

    added auth services

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,15 +1,20 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Post, Body, Res} from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import { AuthLoginDTO, AuthRegisterDTO } from './dto/auth.dto';
+
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Post()
-  create(@Body() dto: CreateAuthDto) {
-    return this.authService.create(dto);
+  @Post('login')
+  create(@Body() dto: AuthLoginDTO) {
+    return this.authService.findOne(dto.username)
+  }
+
+  @Post('registration')
+  registrasi(@Body() body : AuthRegisterDTO){
+    return "Hy saya dari body"
   }
 
   
--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,18 +1,39 @@
 import {IsString, IsEmail, IsNotEmpty, IsOptional} from "class-validator"
 
 export class AuthLoginDTO {
+    @IsNotEmpty()
+    @IsString()
     username : string;
+
+    @IsNotEmpty()
+    @IsString()
     password : string;
 }
 export class AuthRegisterDTO {
+
+    @IsNotEmpty()
+    @IsString()
     username : string;
+
+    @IsNotEmpty()
+    @IsString()
     password : string;
+
+    @IsOptional()
+    @IsNotEmpty()
+    @IsString()
+    name : string;
+
+    @IsOptional()
+    @IsNotEmpty()
+    @IsString()
+    address : string;
+
+    @IsNotEmpty()
+    @IsString()
+    phone : string;
+
+    @IsEmail()
+    email : string;
 }
-export class AuthRefreshDTO {
-    username : string;
-    password : string;
-}
-export class AuthProfileDTO {
-    username : string;
-    password : string;
-}
+
